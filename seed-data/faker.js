faa75e392b8a7d10cfca131b84245e5b711c28c0
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2018-10-25 20:41:23 -0700

    seeding database with csv file

--- seed-data/faker.js
+++ seed-data/faker.js
@@ -1,8 +1,6 @@
 var faker = require('faker');
 var fs = require('fs');
 
-var data = []
-
 var genres = ['horror', 'comedy', 'action', 'drama', 'documentary', 'adult', 'animation'];
 var year = [2017,2000,1996,2002,2018,1992,2010];
 var photos = ['https://s3-us-west-1.amazonaws.com/moovi-photos/0001.jpg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/0002.jpg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/0003.jpeg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/0004.jpg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/0005.jpg',
@@ -10,25 +8,25 @@ var photos = ['https://s3-us-west-1.amazonaws.com/moovi-photos/0001.jpg', 'https
               'https://s3-us-west-1.amazonaws.com/moovi-photos/Image+uploaded+from+iOS.jpg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/download+(1).jpeg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/download+(2).jpeg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/download+(3).jpeg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/download+(4).jpeg', 'https://s3-us-west-1.amazonaws.com/moovi-photos/download+(5).jpeg',
               'https://s3-us-west-1.amazonaws.com/moovi-photos/download.jpeg']
 
-var dataMaker = (num = 100) => {
+var csvMaker = (num = 100) => {
   for(let i = 0; i<num; i++) {
-  	var movie = {};
+  	let movie = {};
   	movie.title = faker.lorem.words();
   	movie.rating = Math.floor(Math.random()*100);
   	movie.trailer = photos[Math.floor(Math.random()*17)]
   	movie.photos = [photos[Math.floor(Math.random()*17)], photos[Math.floor(Math.random()*17)], photos[Math.floor(Math.random()*17)], photos[Math.floor(Math.random()*17)], photos[Math.floor(Math.random()*17)]];
-  	movie.genre = genres[i];
+  	movie.genre = genres[Math.floor(Math.random()*7)];
   	movie.cast = [faker.name.findName(),faker.name.findName(),faker.name.findName(),faker.name.findName(),faker.name.findName()];
   	movie.director = faker.name.findName();
-  	movie.year = year[i];
-  	data.push(movie);
+  	movie.year = year[Math.floor(Math.random()*7)];
+    movie.synopsis = faker.lorem.paragraph();
+
+    let row = `\n${movie.title}, ${movie.rating}, ${movie.genre}, ${movie.director}, ${movie.year}, ${movie.trailer}, [${movie.cast}], ${movie.photos}, ${movie.synopsis}`;
+
+    fs.appendFile('./seed-data/summaries/summaries.csv', row, (err) => {
+      if(err) throw err;
+    })
   }
 }
 
-dataMaker();
-
-fs.writeFile('./seed-data/summaries/summaries.json', JSON.stringify(data), (err) => {
-  if(err) {
-  	console.log(err.message)
-  }
-})
+csvMaker(2);
