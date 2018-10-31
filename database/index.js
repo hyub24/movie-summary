5136763af4161f355917c6db71fa1e0304e35cd4
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2018-10-30 17:34:16 -0700

    working on css

--- database/index.js
+++ database/index.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/movie-summary');
 
-
 const getSummary = (cb) => {
   mongoose.connection.db.collection('summaries', (err, collection) => {
     collection.find().toArray(cb);
