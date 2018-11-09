1c354a9be52e2ed8569535d0cce8b7eceab1477a
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2018-11-09 10:18:13 -0800

    added dockerfile

--- database/index.js
+++ database/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movie-summary');
+mongoose.connect('mongodb://database/movie-summary');
 
 const getSummary = (cb) => {
   mongoose.connection.db.collection('summaries', (err, collection) => {
+    if (err) cb(err);
     collection.find().toArray(cb);
   });
 };
